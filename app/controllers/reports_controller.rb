b1b518fd82347b0b002f922959ed0a15d55b3171
Yee-Ning <yee.ningc@gmail.com>
2014-06-14 18:56:26 -0400

    Google Map Shiz

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,10 @@
 class ReportsController < ApplicationController
   def index
+  	@check_ins = CheckIn.all
+ 
+  	respond_to do |format|
+    	format.html
+    	format.json { render :json => @check_ins.to_json }
+  	end
   end
 end
