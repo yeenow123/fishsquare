282609186415c7633c056ab888b434c5f5d716fc
Andrew Zitek <zitek936@gmail.com>
2014-06-14 22:29:42 -0400

    reports page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,22 +1,39 @@
 class ReportsController < ApplicationController
   def index  	
+
   	@search = CheckIn.search do
 
-  		# search over report types
-  		if params[:report_type]
-    		fulltext params[:report_type]
-    	end
+      # search over report types
+      if params.key?(:report_type)
+        if params[:report_type]
+          fulltext params[:report_type]
+        end
+      end
 
-    	# geo search in radius
-    	if params[:lat] and params[:lng] and params[:radius]
-			 with(:location).in_radius(params[:lat], params[:lng], params[:radius], :bbox => true)
-    	end
+      if params.key?(:lat) and params.key?(:lng) and params.key?(:radius)
+        # geo search in radius
+        if params[:lat] != '' and params[:lng] != '' and params[:radius] != ''
+         with(:location).in_radius(params[:lat], params[:lng], params[:radius], :bbox => true)
+        end
+      end
 
-    	# search date range
-    	if params[:date_start] and params[:date_end]
-  			with(:report_time).between(Time.now-(3600*24), Time.now)
-  			# with(:report_time).greater_than(date)	
-    	end
+      # search date range   
+      if params.key?(:date_start)  
+        if params[:date_start]["(1i)"] && params[:date_start]["(2i)"] && params[:date_start]["(3i)"]
+          date_start = Date.new params[:date_start]["(1i)"].to_i, params[:date_start]["(2i)"].to_i, params[:date_start]["(3i)"].to_i
+        end
+      end
+
+      if params.key?(:date_end)
+        if params[:date_end]["(1i)"] && params[:date_end]["(2i)"] && params[:date_end]["(3i)"]
+          date_end = Date.new params[:date_end]["(1i)"].to_i, params[:date_end]["(2i)"].to_i, params[:date_end]["(3i)"].to_i
+        end
+      end
+
+      if date_start and date_end
+        with(:report_time).greater_than(date_start)
+        with(:report_time).less_than(date_end) 
+      end
 
   	end
   	@check_ins = @search.results
